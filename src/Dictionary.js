bcc9a9c4601924c09d7b32b67a0cfcdde63c00ae
Its-Shio <dkell02eosd@gmail.com>
2025-04-09 13:45:22 -0400

    Added working dictionary functionality

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data.meanings[0]);
+    setResults(response.data);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
-
     const apiKey = "de0e2db99e807aaf2e3te4ed847cc3o3";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
@@ -33,6 +33,7 @@ export default function Dictionary() {
           onChange={handleKeywordChange}
         ></input>
       </form>
+      <Results results={results} />
     </div>
   );
 }
